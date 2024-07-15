72ee80832faefa1638d36c75794a00760325dc03
ethanburmane <ethanburmane@gmail.com>
2024-07-15 14:16:13 -0400

    working character class iteration

--- src/main/java/dk/brics/automaton/Egret.java
+++ src/main/java/dk/brics/automaton/Egret.java
@@ -13,7 +13,6 @@ public class Egret {
         // setting up the automaton
         RegExp regExp = new RegExp(s);
         Automaton auto = regExp.toAutomaton();
-        TransitionTable tt = new TransitionTable(auto);
 
         // initializing visited states list
         ArrayList<State> path = new ArrayList<>();
@@ -23,31 +22,73 @@ public class Egret {
         }
 
         // finding basis strings
-        ArrayList<String> strings = findBasisPath(auto.getInitialState(), path, auto, tt, visited);
+        ArrayList<String> strings = findBasisPath(auto.getInitialState(), path, visited);
 
         // printing basis string
         System.out.println(strings);
     }
 
-    private static void addPathToList(ArrayList<State> path) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < path.size() - 1; i++) {
-            State state = path.get(i);
-            int transitionIndex = 0;
-            while (!state.getSortedTransitions(false).get(transitionIndex).getDest().equals(path.get(i+1))) {
-                transitionIndex++;
+    private static ArrayList<Transition> findTransitions(State state1, State state2) {
+        ArrayList<Transition> transitions = new ArrayList<>();
+        for (Transition t : state1.getTransitions()) {
+            if (t.getDest().equals(state2)) {
+                transitions.add(t);
             }
-            sb.append(state.getSortedTransitions(false).get(transitionIndex).getMin());
+        }
+        return transitions;
+    }
+
+    private static ArrayList<String> addCharacters(Transition transition, ArrayList<String> pathStrings) {
+        ArrayList<String> newPathStrings = new ArrayList<>();
+
+        // if the transition is a range
+        if (transition.getMin() != transition.getMax()) {
+            if (!pathStrings.isEmpty()) {
+                for (String s : pathStrings) {
+                    // replace each string every combination of itself and the range characters
+                    for (char c = transition.getMin(); c <= transition.getMax(); c++) {
+                        newPathStrings.add(s + String.valueOf(c));
+                    }
+                }
+            }
+            else {
+                // add all range characters to the list
+                for (char c = transition.getMin(); c <= transition.getMax(); c++) {
+                    newPathStrings.add(c + "");
+                }
+            }
+        }
+        // if the transition is not a range
+        else {
+            if (!pathStrings.isEmpty()) {
+                pathStrings.replaceAll(s -> s + transition.getMin());
+                newPathStrings.addAll(pathStrings);
+            }
+            else {
+                newPathStrings.add(String.valueOf(transition.getMin()));
+            }
         }
 
-        list.add(sb.toString());
+        return newPathStrings;
+    }
+
+    private static void addPathToList(ArrayList<State> path) {
+        ArrayList<String> pathStrings = new ArrayList<>();
+        for (int i = 0; i < path.size() - 1; i++) {
+            ArrayList<Transition> transitions = findTransitions(path.get(i), path.get(i + 1));
+            for (Transition t : transitions) {
+                pathStrings = addCharacters(t, pathStrings);
+            }
+        }
+
+        list.addAll(pathStrings);
     }
 
     private static ArrayList<State> deepCopy(ArrayList<State> path) {
         return new ArrayList<>(path);
     }
 
-    private static ArrayList<String> findBasisPath(State curr, ArrayList<State> path, Automaton auto, TransitionTable tt, Map<State, Boolean> visited) {
+    private static ArrayList<String> findBasisPath(State curr, ArrayList<State> path, Map<State, Boolean> visited) {
 
         // add state to path
         ArrayList<State> currPath = deepCopy(path);
@@ -68,7 +109,7 @@ public class Egret {
         else if (visited.get(curr)) {
             // next_state = lowest-numbered state that has a transition from curr_state to next_state
             State next = curr.getSortedTransitions(false).get(0).getDest();
-            findBasisPath(next, currPath, auto, tt, visited);
+            findBasisPath(next, currPath, visited);
         }
 
         // else:
@@ -77,7 +118,7 @@ public class Egret {
             List<Transition> transitions = curr.getSortedTransitions(false);
             for (Transition transition : transitions) {
                 State dest = transition.getDest();
-                findBasisPath(dest, currPath, auto, tt, visited);
+                findBasisPath(dest, currPath, visited);
             }
         }
 
--- src/test/java/dk/brics/automaton/EgretTest.java
+++ src/test/java/dk/brics/automaton/EgretTest.java
@@ -5,7 +5,7 @@ import static dk.brics.automaton.Egret.getRegexStrings;
 public class EgretTest {
 
     private static void testEgret() {
-        String regExp = "a[b|c+]";
+        String regExp = "[a-zA-Z0-9]";
         getRegexStrings(regExp);
     }
 
