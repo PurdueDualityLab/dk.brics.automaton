6c5ff574549f53492e2305d60ecca1a2a1d70319
ethanburmane <ethanburmane@gmail.com>
2024-07-29 14:13:09 -0400

    added random character selection from compliment automaton

--- src/main/java/dk/brics/automaton/GenerateStrings.java
+++ src/main/java/dk/brics/automaton/GenerateStrings.java
@@ -1,6 +1,9 @@
 package dk.brics.automaton;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,22 +11,29 @@ public class GenerateStrings {
 
     /**
      * Finds an estimation of all the strings a regular expression can match with
-     * @param s String representation of a regex
+     * @param regExpStr String representation of a regex
      * @param maxNumVisits Max number of visits per state in regex automaton
+     * @param genPositiveStrings Flag for generating either positive strings (true) or negative (false) strings
      * @return ArrayList containing positive strings for the regex
      */
-    public static ArrayList<String> generateStrings(String s, int maxNumVisits) {
-        RegExp re = new RegExp(s);
-        Automaton auto = re.toAutomaton();
-
+    public static Set<String> generateStrings(String regExpStr, int maxNumVisits, boolean genPositiveStrings) {
+        RegExp regExp = new RegExp(regExpStr);
+        Automaton automaton = regExp.toAutomaton();
         ArrayList<State> path = new ArrayList<>();
-        ArrayList<String> positiveStr = new ArrayList<>();
-        traverse(auto.getInitialState(), maxNumVisits, path, positiveStr);
+        Set<String> strings = new HashSet<>();
 
-        return positiveStr;
+        if (genPositiveStrings) {
+            traverse(automaton.getInitialState(), maxNumVisits, path, strings, true);
+        }
+        else {
+            Automaton autoCompliment = automaton.complement();
+            traverse(autoCompliment.getInitialState(), maxNumVisits, path, strings, false);
+        }
+        return strings;
     }
 
-    private static void traverse(State curr, int maxNumVisits, ArrayList<State> path, ArrayList<String> positiveStr) {
+
+    private static void traverse(State curr, int maxNumVisits, ArrayList<State> path, Set<String> strings, boolean sign) {
 
         ArrayList<State> currPath = shallowCopy(path);
         currPath.add(curr);
@@ -31,15 +41,15 @@ public class GenerateStrings {
 
         try {
             if (curr.isAccept()) {
-                addPathToList(currPath, positiveStr);
+                addPathToList(currPath, strings, sign);
             }
             for (Transition t : curr.getTransitions()) {
                 if (t.getDest().numVisits < maxNumVisits) {
                     if (curr.equals(t.getDest())) {
-                        traverse(t.getDest(), maxNumVisits / 2, currPath, positiveStr);
+                        traverse(t.getDest(), maxNumVisits / 2, currPath, strings, sign);
                     }
                     else {
-                        traverse(t.getDest(), maxNumVisits, currPath, positiveStr);
+                        traverse(t.getDest(), maxNumVisits, currPath, strings, sign);
                     }
                 }
             }
@@ -61,20 +71,29 @@ public class GenerateStrings {
      * @param reuseCandidateStr String representation of the reuse candidate regex
      * @return e-similarity score as a float (between 0 and 1)
      */
-    public static float eSimilarity(String truthRegexStr, String reuseCandidateStr) {
+    public static float eSimilarity(String truthRegexStr, String reuseCandidateStr, int maxNumVisits) {
 
-        ArrayList<String> truthPositiveStr = generateStrings(truthRegexStr, 1);
+        Set<String> truthPositiveStr = generateStrings(truthRegexStr, maxNumVisits, true);
+        Set<String> truthNegativeStr = generateStrings(truthRegexStr, maxNumVisits, false);
         Pattern reuseCandidateRegex = Pattern.compile(reuseCandidateStr);
 
         int numPositiveStr = truthPositiveStr.size();
+        int numNegativeStr = truthNegativeStr.size();
         int numMatches = 0;
+        int numRejects = 0;
         for (String s : truthPositiveStr) {
             Matcher match = reuseCandidateRegex.matcher(s);
             if (match.matches()) {
                 numMatches++;
             }
         }
-        int e = 1 - (numMatches / numPositiveStr);
+        for (String s : truthNegativeStr) {
+            Matcher match = reuseCandidateRegex.matcher(s);
+            if (!match.matches()) {
+                numRejects++;
+            }
+        }
+        int e = 1 - ((numMatches + numRejects) / (numPositiveStr + numNegativeStr));
         return 1 - e;
     }
 
@@ -82,17 +101,17 @@ public class GenerateStrings {
         return new ArrayList<>(path);
     }
 
-    public static void addPathToList(ArrayList<State> path, ArrayList<String> positiveStr) {
+    private static void addPathToList(ArrayList<State> path, Set<String> strings, boolean sign) {
         ArrayList<String> pathStrings = new ArrayList<>();
         for (int i = 0; i < path.size() - 1; i++) {
             ArrayList<Transition> transitions = findTransitions(path.get(i), path.get(i + 1));
-            pathStrings = addCharacters(transitions, pathStrings);
+            pathStrings = addCharacters(transitions, pathStrings, sign);
         }
-        positiveStr.addAll(pathStrings);
+        strings.addAll(pathStrings);
     }
 
 
-    public static ArrayList<Transition> findTransitions(State currState, State destState) {
+    private static ArrayList<Transition> findTransitions(State currState, State destState) {
         ArrayList<Transition> transitions = new ArrayList<>();
         for (Transition t : currState.getTransitions()) {
             if (t.getDest().equals(destState)) {
@@ -103,16 +122,9 @@ public class GenerateStrings {
     }
 
 
-    private static ArrayList<String> addCharacters(ArrayList<Transition> transitions, ArrayList<String> pathStrings) {
+    private static ArrayList<String> addCharacters(ArrayList<Transition> transitions, ArrayList<String> pathStrings, boolean sign) {
         ArrayList<String> newPathStrings = new ArrayList<>();
-        ArrayList<Character> charsToAppend = new ArrayList<>();
-
-        // making a list of each character option to append
-        for (Transition t : transitions) {
-            for (char c = t.getMin(); c <= t.getMax(); c++) {
-                charsToAppend.add(c);
-            }
-        }
+        ArrayList<Character> charsToAppend = getCharsToAppend(transitions, sign);
 
         // adding characters
         if (!pathStrings.isEmpty()) {
@@ -127,5 +139,43 @@ public class GenerateStrings {
             }
         }
         return newPathStrings;
+    }
+
+    private static ArrayList<Character> getCharsToAppend(ArrayList<Transition> transitions, boolean sign) {
+        ArrayList<Character> charsToAppend = new ArrayList<>();
+
+        if (sign) {
+            for (Transition t : transitions) {
+                for (char c = t.getMin(); c <= t.getMax(); c++) {
+                    charsToAppend.add(c);
+                }
+            }
+        }
+        else {
+            for (Transition t : transitions) {
+                charsToAppend.add(genCharacters(t));
+            }
+        }
+        return charsToAppend;
+    }
+
+    private static char genCharacters(Transition transition) {
+
+        if (transition.getMin() == transition.getMax()) {
+            return transition.getMin();
+        }
+
+        Random random = new Random();
+        int minRangeValue;
+        int maxRangeValue;
+        if (transition.getMin() > 0x007e || transition.getMax() < 0x0020) {
+            minRangeValue = transition.getMin();
+            maxRangeValue = transition.getMax();
+        } else {
+            minRangeValue = Math.max(transition.getMin(), 0x0020);
+            maxRangeValue = Math.min(transition.getMax(), 0x007e);
+        }
+        int randomCharacter = random.nextInt(maxRangeValue - minRangeValue) + minRangeValue;
+        return (char) randomCharacter;
     }
 }
