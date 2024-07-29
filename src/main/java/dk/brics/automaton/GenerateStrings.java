4faed8b7cf4778ff6cd9491261cd7976a5e58875
Charlie Sale <softwaresale01@gmail.com>
2024-07-29 15:06:58 -0400

    generalize esimilarity function
    Just takes positive and negative strings along with a classifier

--- src/main/java/dk/brics/automaton/GenerateStrings.java
+++ src/main/java/dk/brics/automaton/GenerateStrings.java
@@ -1,10 +1,11 @@
 package dk.brics.automaton;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
-import java.util.regex.Matcher;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class GenerateStrings {
@@ -15,18 +16,31 @@ public class GenerateStrings {
      * @param maxNumVisits Max number of visits per state in regex automaton
      * @param genPositiveStrings Flag for generating either positive strings (true) or negative (false) strings
      * @return ArrayList containing positive strings for the regex
+     * @throws IllegalArgumentException might be due to parsing, or regex is too large to estimate
      */
     public static Set<String> generateStrings(String regExpStr, int maxNumVisits, boolean genPositiveStrings) throws IllegalArgumentException {
         RegExp regExp = new RegExp(regExpStr);
         Automaton automaton = regExp.toAutomaton();
+        return generateStrings(automaton, maxNumVisits, genPositiveStrings);
+    }
+
+    /**
+     * Finds an estimation of all the strings a regular expression can match with
+     * @param regexAuto String representation of a regex
+     * @param maxNumVisits Max number of visits per state in regex automaton
+     * @param genPositiveStrings Flag for generating either positive strings (true) or negative (false) strings
+     * @return ArrayList containing positive strings for the regex
+     * @throws IllegalArgumentException regex is too large to estimate
+     */
+    public static Set<String> generateStrings(Automaton regexAuto, int maxNumVisits, boolean genPositiveStrings) throws IllegalArgumentException {
         ArrayList<State> path = new ArrayList<>();
         Set<String> strings = new HashSet<>();
 
         if (genPositiveStrings) {
-            traverse(automaton.getInitialState(), maxNumVisits, path, strings, true);
+            traverse(regexAuto.getInitialState(), maxNumVisits, path, strings, true);
         }
         else {
-            Automaton autoCompliment = automaton.complement();
+            Automaton autoCompliment = regexAuto.complement();
             traverse(autoCompliment.getInitialState(), maxNumVisits, path, strings, false);
         }
         return strings;
@@ -69,25 +83,44 @@ public class GenerateStrings {
      * @param reuseCandidateStr String representation of the reuse candidate regex
      * @return e-similarity score as a float (between 0 and 1)
      */
-    public static double eSimilarity(String truthRegexStr, String reuseCandidateStr, int maxNumVisits) {
+    public static double eSimilarity(String truthRegexStr, String reuseCandidateStr, int maxNumVisits, boolean fullMatchOrPartialMatch) {
 
         Set<String> truthPositiveStr = generateStrings(truthRegexStr, maxNumVisits, true);
         Set<String> truthNegativeStr = generateStrings(truthRegexStr, maxNumVisits, false);
         Pattern reuseCandidateRegex = Pattern.compile(reuseCandidateStr);
+        Predicate<String> tester = (inputString) -> {
+            if (fullMatchOrPartialMatch) {
+                return reuseCandidateRegex.matcher(inputString).matches();
+            } else {
+                return reuseCandidateRegex.matcher(inputString).find();
+            }
+        };
 
+        return eSimilarity(truthPositiveStr, truthNegativeStr, tester);
+    }
+
+    /**
+     * More generalized esimilarity function. Takes in a set of positive and negative strings from the truth regex
+     * and a predicate that determines if a reuse candidate matches a string or not. The matchClassifier is used
+     * to classify the positive/negative strings, and an esimilarity score is reported
+     *
+     * @param truthPositiveStr Collection of positive/matching strings
+     * @param truthNegativeStr Collection of negative/mis-matching strings
+     * @param matchClassifier A predicate that determines if a reuse candidate matches the provided string
+     * @return esimilarity score
+     */
+    public static double eSimilarity(Collection<String> truthPositiveStr, Collection<String> truthNegativeStr, Predicate<String> matchClassifier) {
         int numPositiveStr = truthPositiveStr.size();
         int numNegativeStr = truthNegativeStr.size();
         int numMatches = 0;
         int numRejects = 0;
-        for (String s : truthPositiveStr) {
-            Matcher match = reuseCandidateRegex.matcher(s);
-            if (match.matches()) {
+        for (String positiveStr : truthPositiveStr) {
+            if (matchClassifier.test(positiveStr)) {
                 numMatches++;
             }
         }
-        for (String s : truthNegativeStr) {
-            Matcher match = reuseCandidateRegex.matcher(s);
-            if (!match.matches()) {
+        for (String negativeStr : truthNegativeStr) {
+            if (!matchClassifier.test(negativeStr)) {
                 numRejects++;
             }
         }
